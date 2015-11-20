06a1cb33c3e43de21d09e5b73082239448f09fdd
naddison1993 <neaddison16@transy.edu>
2015-11-20 10:34:19 -0500

    Made a table view that is able to add rows to the table view.

---
+++ The Yutorial/YutorialDetailViewController.swift
@@ -0,0 +1,44 @@
+//
+//  YutorialDetailViewController.swift
+//  
+//
+//  Created by Nathan Addison on 11/19/15.
+//
+//
+
+import UIKit
+
+class YutorialDetailViewController: UIViewController {
+
+    @IBOutlet weak var yutorialName: UITextField!
+    var name: String = ""
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if (segue.identifier == "doneSegue") {
+            name = yutorialName.text
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
