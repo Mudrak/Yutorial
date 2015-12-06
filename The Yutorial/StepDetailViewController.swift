7103d760556962359c7367a7f742c1dee35a6aa4
Mudrak <ejmudrak17@transy.edu>
2015-12-05 23:02:53 -0500

    #9: Checklist Model
    Steps and titles transfer to individual checklist pages, and table view
    is contained by 3rd view controller. Toggle is next

--- The Yutorial/StepDetailViewController.swift
+++ The Yutorial/StepDetailViewController.swift
@@ -8,12 +8,26 @@
 
 import UIKit
 
-class StepDetailViewController: UITableViewController {
+class StepDetailViewController: UIViewController, UITableViewDataSource {
     
-    var stepInformation: String! 
+    
+    @IBOutlet var checklistTable: UITableView!
+    @IBOutlet weak var stepDetailImage: UIImageView!
+    @IBOutlet weak var stepDetailTitle: UILabel!
+    
+    var stepInformation: String!
+    var stepNumber: UIImage!
+    
+    var checklistItems: [String!] = ["List of checklist items", "Enter substeps and data here", "From notes to camera roll images","Add specific info for complex tasks"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        stepDetailTitle.text = stepInformation
+        stepDetailImage.image = stepNumber
+        stepDetailTitle.textColor = UIColor(red: 0.0/255.0, green: 160.0/255.0, blue: 135.0/255.0, alpha: 1.0)
+        stepDetailTitle.font = UIFont(name: "Montserrat-Regular", size: 35)
+        navigationItem.title = "Step Details"
 
         // Do any additional setup after loading the view.
     }
@@ -23,7 +37,39 @@ class StepDetailViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Only one section necessary
+        return checklistItems.count
+    }
 
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+            let cell = self.checklistTable.dequeueReusableCellWithIdentifier("checklistItem") as! checklistCell
+
+            cell.checklistLabel.text = checklistItems[indexPath.row]
+            cell.checklistLabel.textColor = UIColor(red: 0.0/255.0, green: 160.0/255.0, blue: 135.0/255.0, alpha: 1.0)
+            cell.checklistLabel.font = UIFont(name: "Montserrat-Regular", size: 25)
+        
+            let cellPattern = indexPath.row % 2
+            // Alternate cell colors
+            if (cellPattern == 0) {
+                // Color it teal
+                cell.checklistLabel.textColor = UIColor.whiteColor()
+                cell.backgroundColor = UIColor(red: 0.0/255.0, green: 188.0/255.0, blue: 158.0/255.0, alpha: 1.0)
+            }
+            else {
+                cell.checklistLabel.textColor = UIColor(red: 0.0/255.0, green: 188.0/255.0, blue: 158.0/255.0, alpha: 1.0)
+                cell.backgroundColor = UIColor.whiteColor()
+            }
+        return cell
+    }
+
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath){
+        let selectedCell = self.checklistTable.cellForRowAtIndexPath(indexPath) as! checklistCell
+        
+        selectedCell.backgroundColor = UIColor(red: 78/255, green: 205/255, blue: 184/255, alpha: 1.0)
+        selectedCell.checklistLabel.textColor = UIColor.whiteColor()
+    }
+    
     /*
     // MARK: - Navigation
 
---
+++ The Yutorial/checklistCell.swift
@@ -0,0 +1,26 @@
+//
+//  checklistCell.swift
+//  
+//
+//  Created by admin on 12/5/15.
+//
+//
+
+import UIKit
+
+class checklistCell: UITableViewCell {
+
+    @IBOutlet weak var checklistLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
